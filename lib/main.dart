03135aca60ec3c428758187f5d53baa9e496a145
Abdullah Kazmi <43041721+AbdullahKazmi31@users.noreply.github.com>
2020-03-25 00:54:30 +0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/loading.dart';
+import 'package:world_time/pages/choose_location.dart';
+
+void main() {
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.dumpErrorToConsole(details);
+    if (kReleaseMode)
+      exit(1);
+  };
+
+  runApp(MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+
+      }
+  ));
+}
